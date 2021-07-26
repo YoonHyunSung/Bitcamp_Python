54fab8a324790669c35363fd8ee01e11f8c847e4
YoonHyunSung <gustjd6495@gmail.com>
2021-07-26 17:50:19 +0900

    0726

--- modu/template/__init__.py
+++ modu/template/__init__.py
@@ -1,9 +1,10 @@
 from common.menu import print_menu
 from modu.template.basic_plot import plot_show, plot_two_list_show, plot_label, plot_marker, scatter
+from modu.template.changed_temperature_on_my_birthday import ChangedTemperaturesOnMyBirthday
 
 if __name__ == '__main__':
     while 1:
-        menu = print_menu(['exit', 'Plot Show','Plot tow list show','plot_label','plot_marker','scatter'])
+        menu = print_menu(['exit', 'Plot Show','Plot tow list show','plot_label','plot_marker','scatter','Blank','ChangedTemperaturesOnMyBirthday'])
         if menu == 0:
             break
         elif menu == 1:
@@ -16,3 +17,8 @@ if __name__ == '__main__':
             plot_marker()
         elif menu == 5:
             scatter()
+        elif menu == 6:
+            pass
+        elif menu == 7:
+            ChangedTemperaturesOnMyBirthday().processing()
+            pass
